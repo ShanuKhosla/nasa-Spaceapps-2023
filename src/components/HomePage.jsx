a2bc77fa396552b730af753e44a5290ef6d64522
Shantanu <shantanukhoslas@gmail.com>
2023-06-06 12:40:12 +0530

    back to top button added to homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import Navbar from './Navbar';
 import HeroSection from './HeroSection';
 import SpaceAppsRecap from './SpaceAppsRecap';
@@ -8,6 +9,30 @@ import CTA from './CTA';
 import Footer from './Footer';
 
 const HomePage = () => {
+  const [showBackToTop, setShowBackToTop] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      setShowBackToTop(scrollTop > 1200);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleBackToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const buttonVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
   return (
     <div className="overflow-x-hidden">
       <div
@@ -22,11 +47,23 @@ const HomePage = () => {
         <Navbar />
         <HeroSection />
       </div>
-        <SpaceAppsRecap />
+      <SpaceAppsRecap />
       <ParticipationDetails />
       <Timeline />
       <CTA />
       <Footer />
+
+      {showBackToTop && (
+        <motion.img
+          onClick={handleBackToTop}
+          src="/arrow-circle-up.svg"
+          className="w-[50px] fixed bottom-4 right-4 bg-blue-500 hover:bg-blue-600 text-white rounded-full"
+          initial="hidden"
+          animate="visible"
+          variants={buttonVariants}
+          transition={{ duration: 0.9, ease: 'easeInOut' }}
+        />
+      )}
     </div>
   );
 };
